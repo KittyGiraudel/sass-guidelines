16637db8be6e2be344097ebdc49970ef23ccf95c
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-02-24 17:39:11 +0100

    Restructured JS folder

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,9 +1,12 @@
+import App from './App';
+import Sidebar from './Sidebar';
+
 document.addEventListener('DOMContentLoaded', function (event) {
-  var app = new window.App({
+  var app = new App({
     languagePicker: document.getElementById('language-picker')
   });
 
-  var sidebar = new window.Sidebar({
+  var sidebar = new Sidebar({
     addOffsetView: 50,
     headings: document.querySelectorAll('.chapter:not(.toc) > h1[id]'),
     tableOfContents: document.querySelector('.toc'),
