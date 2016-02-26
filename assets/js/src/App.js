24d1ebdd68641262a78919ed2d10ae4ab41cc154
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-02-26 14:44:48 +0100

    Improved JavaScript code quality

--- assets/js/src/App.js
+++ assets/js/src/App.js
@@ -1,84 +1,76 @@
-import h from './helpers';
+/* globals $ */
 
-var App = function (config) {
-  this.languagePicker = config.languagePicker;
-  this.language = document.documentElement.getAttribute('lang');
-  this.isLargerThanMobile = h.evalClientResolution(680);
-  this.chapters = document.querySelectorAll('.chapter:not(.toc)');
-  this.idIndex = 100;
-  this.editSvg = document.querySelector('meta[name="svg-pencil-icon"]').getAttribute('content');
-  this.linkSvg = document.querySelector('meta[name="svg-link-icon"]').getAttribute('content');
+export default (function () {
+  'use strict';
 
-  this.addChapterLinks();
-  this.bindUI();
-  this.addEvents();
-};
+  // DOM queries
+  var language = document.documentElement.getAttribute('lang');
+  var languagePicker = $('#language-picker')[0];
+  var editSvg = $('meta[name="svg-pencil-icon"]')[0].getAttribute('content');
+  var linkSvg = $('meta[name="svg-link-icon"]')[0].getAttribute('content');
+  var chapters = $('.chapter:not(.toc)');
+  var syntaxToggle = $('input[name="syntax"]');
 
-App.prototype.addEvents = function () {
-  addEventListener('hashchange', this.fixSkipLinks.bind(this), false);
-  this.languagePicker.addEventListener('change', this.redirectUrl, false);
-};
+  // Internal variables
+  var idIndex = 100;
 
-App.prototype.bindUI = function () {
-  var input = document.querySelectorAll('input[name="syntax"]');
+  var sanitizeSVG = function (svg) {
+    return svg
+      .replace(/title\-\d+/g, ('title-' + idIndex++))
+      .replace(/desc\-\d+/g, ('desc-' + idIndex++));
+  };
 
-  Array.prototype.slice.call(input)
-    .forEach(function (element) {
-      element.addEventListener('click', function (event) {
-        var fn = element.value === 'sass' ? 'add' : 'remove';
-        document.body.classList[fn]('sass');
-      });
-    });
-};
+  var getEditLink = function (chapter) {
+    var id = chapter.id.split('chapter-')[1];
+    var link = document.createElement('a');
+    link.href = 'https://github.com/HugoGiraudel/sass-guidelines/edit/gh-pages/' + language + '/_' + id + '.md';
+    link.innerHTML = sanitizeSVG(editSvg);
+    link.setAttribute('class', 'chapter__edit button-ui');
+    link.setAttribute('target', '_blank');
 
-App.prototype.getEditLink = function (chapter) {
-  var id = chapter.id.split('chapter-')[1];
-  var link = document.createElement('a');
-  link.href = 'https://github.com/HugoGiraudel/sass-guidelines/edit/gh-pages/' + this.language + '/_' + id + '.md';
-  link.innerHTML = this.editSvg.replace(/title\-\d+/g, ('title-' + this.idIndex++));
-  link.setAttribute('class', 'chapter__edit button-ui');
-  link.setAttribute('target', '_blank');
+    return link;
+  };
 
-  return link;
-};
+  var getAnchorLink = function (chapter) {
+    var heading = chapter.querySelector('h1[id]');
+    var link = document.createElement('a');
+    link.href = '#' + heading.id;
+    link.innerHTML = sanitizeSVG(linkSvg);
+    link.setAttribute('class', 'chapter__link button-ui');
 
-App.prototype.getAnchorLink = function (chapter) {
-  var heading = chapter.querySelector('h1[id]');
-  var link = document.createElement('a');
-  link.href = '#' + heading.id;
-  link.innerHTML = this.linkSvg.replace(/title\-\d+/g, ('title-' + this.idIndex++));
-  link.setAttribute('class', 'chapter__link button-ui');
+    return link;
+  };
 
-  return link;
-};
+  var fixSkipLinks = function () {
+    var element = document.getElementById(window.location.hash.substring(1));
 
-App.prototype.addChapterLinks = function () {
-  var that = this;
-  var chapters = Array.prototype.slice.call(that.chapters);
+    if (element) {
+      if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+        element.tabIndex = -1;
+      }
+      element.focus();
+    }
+  };
 
+  var redirectUrl = function (event) {
+    window.location.href = this.value;
+  };
+
+  // Binding events
+  window.on('hashchange', fixSkipLinks, false);
+  languagePicker.on('change', redirectUrl, false);
+  syntaxToggle.on('click', function (event) {
+    var fn = this.value === 'sass' ? 'add' : 'remove';
+    document.body.classList[fn]('sass');
+  });
+
+  // Add chapter buttons
   chapters.forEach(function (chapter) {
     var wrapper = document.createElement('div');
     wrapper.setAttribute('class', 'button-wrapper chapter__buttons');
-    wrapper.appendChild(that.getAnchorLink(chapter));
-    wrapper.appendChild(that.getEditLink(chapter));
+    wrapper.appendChild(getAnchorLink(chapter));
+    wrapper.appendChild(getEditLink(chapter));
     var heading = chapter.querySelector('h1[id]');
     heading.parentNode.insertBefore(wrapper, heading.nextSibling);
   });
-};
-
-App.prototype.fixSkipLinks = function (event) {
-  var element = document.getElementById(location.hash.substring(1));
-
-  if (element) {
-    if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
-      element.tabIndex = -1;
-    }
-    element.focus();
-  }
-};
-
-App.prototype.redirectUrl = function (event) {
-  window.location.href = this.value;
-};
-
-export default App;
+}());
--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,17 +1,10 @@
-import App from './App';
-import Sidebar from './Sidebar';
+/* globals Modal */
+
+import '../vendor/bling';
+import '../vendor/accessible-modal-dialog';
+import './app';
+import './sidebar';
 
 document.addEventListener('DOMContentLoaded', function (event) {
-  var app = new App({
-    languagePicker: document.getElementById('language-picker')
-  });
-
-  var sidebar = new Sidebar({
-    addOffsetView: 50,
-    headings: document.querySelectorAll('.chapter:not(.toc) > h1[id]'),
-    tableOfContents: document.querySelector('.toc'),
-    footer: document.querySelector('.footer')
-  });
-
-  var aside = new Modal(document.getElementById('options-panel'));
+  new Modal(document.getElementById('options-panel')); // eslint-disable-line
 });
