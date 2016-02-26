887f0ac1dc43f1412d486b5041ee1f6f0ec893e4
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-02-26 14:41:19 +0100

    Added bling.js in js/vendor

---
+++ assets/js/vendor/bling.js
@@ -0,0 +1,16 @@
+// bling.js
+// https://gist.github.com/paulirish/12fb951a8b893a454b32
+
+window.$ = document.querySelectorAll.bind(document);
+
+Node.prototype.on = window.on = function (name, fn) {
+  this.addEventListener(name, fn);
+};
+
+NodeList.prototype.__proto__ = Array.prototype;
+
+NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
+  this.forEach(function (elem, i) {
+    elem.on(name, fn);
+  });
+};
