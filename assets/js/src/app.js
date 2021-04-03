c09880485de4ddd5ed1f8b23f26c81ae26c92de8
Hugo Giraudel <kitty.giraudel@gmail.com>
2021-04-03 10:55:46 +0200

    Disable service worker in development

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -104,22 +104,4 @@
     initialiseOptionsPanel()
     initialiseChapters()
   })
-
-  // Install service worker
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('/service-worker.js', { scope: '/' })
-      .then(function (registration) {
-        console.log(
-          '%cserviceworker:registration', 'color:green',
-          `successful with scope: ${registration.scope}`
-        )
-      })
-      .catch(function (error) {
-        console.error(
-          '%cserviceworker:registration', 'color:red',
-          'failed: ', error
-        )
-      })
-  }
 }())
---
+++ assets/js/src/sw.js
@@ -0,0 +1,21 @@
+(function () {
+  'use strict'
+
+  // Install service worker
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/service-worker.js', { scope: '/' })
+      .then(function (registration) {
+        console.log(
+          '%cserviceworker:registration', 'color:green',
+          `successful with scope: ${registration.scope}`
+        )
+      })
+      .catch(function (error) {
+        console.error(
+          '%cserviceworker:registration', 'color:red',
+          'failed: ', error
+        )
+      })
+  }
+})()
